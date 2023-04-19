c035aea66d19461b2716eb7ba87cb65707fa223a
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-04-19 15:12:54 +0500

    Added mat-side-nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 import { MaterialModule } from './material.module'
+import { SideNavComponent } from './side-nav/side-nav.component'
 
 @NgModule({
-  declarations: [AppComponent, CurrentWeatherComponent],
+  declarations: [AppComponent, CurrentWeatherComponent, SideNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
