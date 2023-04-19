245341e1bbe52f57f4cd779769a1ed469e6bf3be
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-04-19 15:42:40 +0500

    It should be fixed material styling bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,28 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      <mat-toolbar color="primary">
+        <span>LocalCast Weather</span>
+      </mat-toolbar>
+
+      <div>Your city, your forecast, right now!</div>
+      <div fxLayout="row">
+        <div fxFlex></div>
+        <mat-card fxFlex="300px">
+          <mat-card-header>
+            <mat-card-title>Current Weather</mat-card-title>
+          </mat-card-header>
+          <mat-card-content>
+            <app-current-weather></app-current-weather>
+          </mat-card-content>
+        </mat-card>
+      </div>
+      <div fxFlex></div>
+    </div>
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'local-weather-app'
+}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav'
@@ -11,6 +12,7 @@ const modules = [
   MatIconModule,
   MatSidenavModule,
   MatListModule,
+  MatCardModule,
 ]
 
 @NgModule({
