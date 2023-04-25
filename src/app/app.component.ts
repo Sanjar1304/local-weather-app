918f589fe0f921fb63a8994ad6fb317318458cf1
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-04-25 16:26:32 +0500

    First resetted changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,32 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <mat-toolbar color="primary">
+        <span>LocalCast Weather</span>
+      </mat-toolbar>
+
+      <div class="title-card">
+        <div class="body-title vertical-margin">Your city, your forecast, right now!</div>
+
+        <div class="mat-card-wrapper">
+          <mat-card>
+            <mat-card-header class="mat-typography">
+              <mat-card-title>
+                <div class="mat-headline">Current Weather</div>
+              </mat-card-title>
+            </mat-card-header>
+            <mat-card-content>
+              <app-current-weather></app-current-weather>
+            </mat-card-content>
+          </mat-card>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'local-weather-app'
+}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
-const modules = [MatButtonModule, MatToolbarModule, MatIconModule]
+const modules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatCardModule,
+]
 
 @NgModule({
   declarations: [],
