567654a123f6d9389bf1b8331c41fb9a4f3b123e
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-01 14:15:53 +0500

    Added ondestroy lifecycle for unsubscribe

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { Subscription } from 'rxjs'
 
 import { ICurrentWeather } from '../interfaces'
 import { WeatherService } from '../weather/weather.service'
@@ -8,13 +9,16 @@ import { WeatherService } from '../weather/weather.service'
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
-export class CurrentWeatherComponent implements OnInit {
+export class CurrentWeatherComponent implements OnInit, OnDestroy {
   @Input() current!: ICurrentWeather
+  currentWeatherSubscription!: Subscription
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    this.weatherService.currentWeathers$.subscribe((data) => (this.current = data))
+    this.currentWeatherSubscription = this.weatherService.currentWeathers$.subscribe(
+      (data) => (this.current = data)
+    )
   }
 
   getOrdinal(date: number) {
@@ -22,5 +26,9 @@ export class CurrentWeatherComponent implements OnInit {
     return n > 0
       ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10]
       : ''
+  }
+
+  ngOnDestroy(): void {
+    this.currentWeatherSubscription.unsubscribe()
   }
 }
